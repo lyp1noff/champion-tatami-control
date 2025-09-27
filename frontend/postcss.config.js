7b8ce8cc44f9742e1f1e99f3a02fbe87ef18b23b
lyp1noff <lyp1noff@gmail.com>
2025-09-27 15:31:04 +0100

    Transpile oklch/lab to rgb for Chrome 109 support

---
+++ frontend/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+    "postcss-color-functional-notation": { preserve: false },
+    "@csstools/postcss-oklab-function": { preserve: false },
+  },
+};
